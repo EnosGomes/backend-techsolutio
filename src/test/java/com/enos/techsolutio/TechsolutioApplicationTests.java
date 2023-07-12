fcc41e47d62cd02233c6f53f0bda092f58590777
EnosGomes <enoskratos@hotmail.com>
2023-07-12 08:19:05 -0300

    commit inicial

---
+++ src/test/java/com/enos/techsolutio/TechsolutioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enos.techsolutio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechsolutioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
